01d788a94af3839d5e3f4eee2bc8e50dbfedd141
cgesgin <cevher-5@hotmail.com>
2023-06-04 15:14:11 +0300

    initial commit

---
+++ src/main/java/com/cgesgin/locationmanagmentapi/LocationManagmentApiApplication.java
@@ -0,0 +1,13 @@
+package com.cgesgin.locationmanagmentapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationManagmentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationManagmentApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cgesgin/locationmanagmentapi/LocationManagmentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cgesgin.locationmanagmentapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationManagmentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
